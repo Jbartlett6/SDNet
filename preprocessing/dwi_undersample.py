441b4c26ad1f708b8386480b90662286cf01f2aa
Joe Bartlett <j_j_bartlett@hotmail.co.uk>
2024-01-15 16:46:21 +1100

    Adding pre-processing to the repo

---
+++ preprocessing/dwi_undersample.py
@@ -0,0 +1,174 @@
+import torch
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0],'..'))
+import nibabel as nib
+import sys 
+
+class UndersampleDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, save_dir):
+        
+        #Initialising the parameters for the dataset class.
+        self.data_path = data_path
+        self.save_dir = save_dir
+
+        self.undersample_val = 9
+        self.save_folder='undersampled_fod'
+
+        #Setting the field strength specific parameters
+        self.diffusion_dir = 'Diffusion'
+        self.shell_number = 4
+        self.data_file = 'normalised_data.nii.gz'
+
+        #Calculating the mask list and keep lists (using this function here will only work when a constant undersampling pattern is used)
+        self.mask_list, self.keep_list= self.sample_lists()
+
+        #Creating the data path and the mask path.
+        dwi_path = os.path.join(data_path, 'data.nii.gz')
+        
+        #Loading the data for the subject
+        image = nib.load(dwi_path)
+        self.head = image.header
+        self.aff = image.affine
+        print('Loading image data')
+        self.image = torch.tensor(image.get_fdata())
+        
+        #os.mkdir(path = data_path+'/'+subject+'/T1w/Diffusion/undersampled_fod_resptest_1')
+
+    def __len__(self):
+        return int(torch.sum(torch.tensor(self.mask)))
+    
+
+    def data_save(self):
+        '''
+        When a constant sampling pattern is used the keep list and mask list can be defined at initalisation as they will be constant for every iteration. However if 
+        some aspect of random sampling is used then the keep lists and mask lists will have to be defined in the get item function.
+        '''
+        print('Saving data')
+        im_usamp = nib.Nifti1Image(self.image[:,:,:,self.keep_list].float().detach().numpy(), affine=self.aff)
+        save_path = os.path.join(self.save_dir, 'data.nii.gz')
+
+        nib.save(im_usamp, save_path)
+        print('Finished saving data')
+
+    def all_save(self):
+        self.data_save()
+        self.bval_save()
+        self.bvec_save()
+
+    def sample_lists(self):
+        '''
+        A function which returns two lists - the mask_list, which is the q-space volumes to be masked,
+        and keep_list for the q-space volumes not to be masked. This will be used in the __getitem__ function 
+        of this class.
+        '''
+
+        self.bvals = self.bval_extract()
+
+        b0_list = []
+        b1000_list = []
+        b2000_list = []
+        b3000_list = []
+    
+        for i in range(len(self.bvals)):
+            if self.bvals[i] <20:
+                b0_list.append(i)
+            elif 980<self.bvals[i]<1020:
+                b1000_list.append(i)
+            elif 1980<self.bvals[i]<2020:
+                b2000_list.append(i)
+            elif 2980<self.bvals[i]<3020:
+                b3000_list.append(i)
+
+        mask_ind = []
+        keep_ind = []
+        for i in range(len(b1000_list)):
+            if i >= self.undersample_val:
+                mask_ind.append(i)
+            else:
+                keep_ind.append(i)
+        print(keep_ind)
+
+        #Uncomment this for the multi-shell version 
+        keep_list = torch.tensor([b1000_list[i]for i in keep_ind] +
+                                [b2000_list[i]for i in keep_ind] +
+                                [b3000_list[i]for i in keep_ind] +
+                                b0_list[:3])
+
+        mask_list = torch.tensor([])
+    
+        return mask_list, keep_list
+    
+    def bval_extract(self):
+        '''
+        Input:
+            subj (str) - The subject whose bvals you want to extract.
+            path (str) - The path to where the data is located (subjects location).
+        
+        Desc:
+            A function to extract the bvalues from the file they are located in and to return them as a list of values.
+        '''
+        
+
+        path = os.path.join(self.data_path, 'bvals')
+        bvals = open(path, 'r')
+        
+        bvals_str = bvals.read()
+        bvals = [int(b) for b in bvals_str.split()]
+        #print('Finished saving bvals')
+        return bvals
+
+    def bvec_save(self):
+        print('Saving bvectors')
+        #Undersampled bvector calculation.
+        path = os.path.join(self.data_path, 'bvecs')
+        
+        with open(path ,'r') as temp:
+            bvecs = temp.read()
+
+        bvecsxyz = bvecs.split('\n')
+        bvecsxyz.pop(3)
+
+        xvals = [x for x in bvecsxyz[0].split()]
+        yvals = [y for y in bvecsxyz[1].split()]
+        zvals = [z for z in bvecsxyz[2].split()]
+
+        #Undersampled bvectors
+        xvals_new = [xvals[ind] for ind in self.keep_list]
+        yvals_new = [yvals[ind] for ind in self.keep_list]
+        zvals_new = [zvals[ind] for ind in self.keep_list]
+
+        xvals_str = ' '.join(xvals_new)
+        yvals_str = ' '.join(yvals_new)
+        zvals_str = ' '.join(zvals_new)
+
+        bvecs_string = '\n'.join((xvals_str,yvals_str,zvals_str))
+       
+        save_path = os.path.join(self.save_dir, 'bvecs')
+        
+        with open(save_path, 'w') as temp:
+            temp.write(bvecs_string)
+        print('Finished Saving bvectors')
+
+
+    
+    def bval_save(self):
+        print('Saving bvals')
+        ##Bvalues
+        new_bvals = [str(self.bvals[ind]) for ind in self.keep_list]
+        bvals_string = ' '.join(new_bvals)
+
+        save_path = os.path.join(self.save_dir, 'bvals')
+
+        with open(save_path,'w') as temp:
+            temp.write(bvals_string)
+        print('Finished saving bvals')
+
+
+if __name__ == '__main__':
+    print('HW')
+    diffusion_dir = '/media/duanj/F/joe/hcp_2/100206_copy/T1w/Diffusion'
+    save_dir = '/media/duanj/F/joe/hcp_2/100206_copy/T1w/Diffusion/undersampled_fod'
+    UspDset = UndersampleDataset(diffusion_dir, save_dir)
+    UspDset.all_save()
+    print('HW')
---
+++ preprocessing/preprocess_py.py
@@ -0,0 +1,71 @@
+import dwi_undersample as dwiusamp
+
+import subprocess 
+import os
+
+def fully_sampled_FOD(path):
+    # Fully sampled FOD
+    # subprocess.run(['dwi2response', 'dhollander', os.path.join(path, 'data.nii.gz'), 
+    #                 os.path.join(path, 'wm_response.txt'), os.path.join(path, 'gm_response.txt'), 
+    #                 os.path.join(path, 'csf_response.txt'), '-fslgrad', 
+    #                 os.path.join(path, 'bvecs'), os.path.join(path, 'bvals')])
+    
+    
+    # subprocess.run(['dwi2fod', '-fslgrad', os.path.join(path, 'bvecs'), os.path.join(path, 'bvals'),
+    #                 'msmt_csd', os.path.join(path, 'data.nii.gz'), os.path.join(path, 'wm_response.txt'),
+    #                 os.path.join(path, 'wmfod.nii.gz'), os.path.join(path, 'gm_response.txt'), os.path.join(path, 'gm.nii.gz'), 
+    #                 os.path.join(path, 'csf_response.txt'), os.path.join(path, 'csf.nii.gz')])
+    
+    # 5 tissue segmentation
+    subprocess.run(['5ttgen', 'fsl', os.path.join(path, '..', 'T1w_acpc_dc_restore_1.25.nii.gz'), os.path.join(path, '..', '5ttgen.nii.gz'), '-nocrop'])
+    subprocess.run(['mrconvert', os.path.join(path, '..', '5ttgen.nii.gz', '-coord', '3', '2', os.path.join(path,'..','white_matter_mask.nii.gz'))])
+
+    # FOD segmentation
+    subprocess.run(['fod2fixel', '-afd', 'afd.mif', '-peak_amp', 'peak_amp.mif', os.path.join(path, 'wmfod.nii.gz'), 
+                    os.path.join(path, 'fixel_directory')])
+    
+    subprocess.run(['fixel2voxel', '-number', '11', os.path.join(path, 'fixel_directory', 'peak_amp.mif'), 
+                    'none', os.path.join(path, 'fixel_directory', 'peak_amp_im.mif')])
+    
+    subprocess.run(['fixel2voxel', '-number', '11', os.path.join(path, 'fixel_directory', 'afd.mif'), 'none', 
+                    os.path.join(path, 'fixel_directory', 'afd_im.mif')])
+
+def undersampled_FOD(path):
+    UspDset = dwiusamp.UndersampleDataset(path, os.path.join(path, 'undersampled_fod'))
+    UspDset.all_save()
+
+    #Normalising the already undersampled data so the maximum value is 1.
+    subprocess.run(['dwinormalise', 'individual', os.path.join(path, 'undersampled_fod', 'data.nii.gz'), 
+                    os.path.join(path, 'nodif_brain_mask.nii.gz'), os.path.join(path, 'undersampled_fod', 'normalised_data.nii.gz'), 
+                    '-fslgrad', os.path.join(path, 'undersampled_fod', 'bvecs'), os.path.join(path, 'undersampled_fod', 'bvals'),
+                    '-intensity', '1'])
+    
+    # Calculating the undersampled FODs
+    subprocess.run(['dwi2response', 'dhollander', os.path.join(path, 'undersampled_fod', 'normalised_data'), os.path.join(path, 'undersampled_fod', 'wm_response.txt'),
+                    os.path.join(path, 'undersampled_fod', 'gm_response.txt'), os.path.join(path, 'undersampled_fod', 'csf_response.txt'), '-fslgrad',
+                    os.path.join(path, 'undersampled_fod', 'bvecs'), os.path.join(path, 'undersampled_fod', 'bvals')])
+    
+    subprocess.run(['dwi2fod', '-fslgrad', os.path.join(path, 'undersampled_fod', 'bvecs'), os.path.join(path, 'undersampled_fod', 'bvals'),
+                 'msmt_csd', os.path.join(path, 'undersampled_fod', 'normalised_data.nii.gz'), os.path.join(path, 'undersampled_fod', 'wm_response.txt'),
+                 os.path.join(path, 'undersampled_fod', 'wm.nii.gz'), os.path.join(path, 'undersampled_fod', 'gm_response.txt'), os.path.join(path, 'undersampled_fod', 'gm.nii.gz'),
+                 os.path.join(path, 'undersampled_fod', 'csf_response.txt'), os.path.join(path, 'undersampled_fod', 'csf_response.txt')])
+    
+    return 0
+
+
+    
+def reset_HCP_dir(path):
+
+    os.remove(os.path.join(path, 'wm_response.txt'))
+    os.remove(os.path.join(path, 'gm_response.txt'))
+    os.remove(os.path.join(path, 'csf_response.txt'))
+
+    os.remove(os.path.join(path, 'wmfod.nii.gz'))
+    os.remove(os.path.join(path, 'gm.nii.gz'))
+    os.remove( os.path.join(path, 'csf.nii.gz'))
+     	                
+if __name__ == '__main__':
+    diffusion_dir = '/mnt/d/Project_1_SDNet/SDNet_data/100610_non_preproc/T1w/Diffusion'
+    print(diffusion_dir)
+    # fully_sampled_FOD(diffusion_dir)
+    undersampled_FOD()

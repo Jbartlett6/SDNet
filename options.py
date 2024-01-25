6f0d72c6943c03f3d55e356de71c4fa17457117f
JBartlett6 <j_j_bartlett@hotmail.co.uk>
2024-01-25 23:22:42 +0000

    temp options change

--- options.py
+++ options.py
@@ -1,17 +1,13 @@
 import torch
 import math
 
-#with open(os.path.join('config', 'fodnet_config.yml')) as f:
-     #config = yaml.load(f, yaml.loader.SafeLoader)
-#print(config)
-
 class NetworkOptions():
     def __init__(self):
         ### General ###
-        self.experiment_name = 'train_init_add_test_1'  # Directory name to be stored in the checkpoint directory. **General**
+        self.experiment_name = 'debugging'  # Directory name to be stored in the checkpoint directory. **General**
         self.model_name = 'best_model.pth'  # Working model name - will be stored within the experiment name directory found within the ** General **
                                             # checkpoints directory. 
-        self.continue_training = True      # Whether to continue training using existing model weights
+        self.continue_training = False      # Whether to continue training using existing model weights
 
         self.inference=False
         self.perform_inference=False
@@ -22,12 +18,12 @@ class NetworkOptions():
         self.warmup_iter = 10000             # Number of iterations after which the network stops warming up.
         self.batch_size = 256               # Training batch_size.
         
-        self.val_freq = 100                 # How often (iterations) to run the validation loop inside the training loop
+        self.val_freq = 2                 # How often (iterations) to run the validation loop inside the training loop
         self.val_iters = 10                 # How many iterations of validation data to loop through when the validation loop is called.
         
         # Stopping Conditions
         self.epochs = 100                   # Maxium number of epochs
-        self.iteration_limit = math.inf     # Maximum number of iterations (training updates)
+        self.iteration_limit = 202505     # Maximum number of iterations (training updates)
         self.lr_decay_limit = math.inf      # The number of times to decay the learning rate 
         self.lr_decay_factor = 1            # The learning rate decay factor
 
@@ -36,8 +32,8 @@ class NetworkOptions():
         self.val_workers = 8 # Number of workers used for the validation dataloader. 
 
         #Early Stopping Parameters
-        self.early_stopping = True          # Whether to include early stopping in the network.
-        self.early_stopping_threshold = 20  # When early_stopping_counter reaches this value training will stop. This counter is updated every validation loop, therefore
+        self.early_stopping = False          # Whether to include early stopping in the network.
+        self.early_stopping_threshold = math.inf  # When early_stopping_counter reaches this value training will stop. This counter is updated every validation loop, therefore
                                             # training will be stopped due to early stopping when the network hasn't improved for early_stopping_threshold validation loops. 
 
         ### Network ###
@@ -52,47 +48,26 @@ class NetworkOptions():
         # self.data_dir = '/media/duanj/F/joe/hcp_2' # Data directory see github repo for more details.
         self.data_dir = '/bask/projects/d/duanj-ai-imaging/jxb1336/hcp'
 
-        self.train_subject_list = ['100206'] #, # Subjects to be used for training
-                    # '100307',
-                    # '100408',
-                    # '100610',
-                    # '101006',
-                    # '101107',
-                    # '101309',
-                    # '101410',
-                    # '101915',
-                    # '102311',
-                    # '102513',
-                    # '102614',
-                    # '102715',
-                    # '102816',
-                    # '103010',
-                    # '103111',
-                    # '103212',
-                    # '103414',
-                    # '103515',
-                    # '103818']
-
-        # self.train_subject_list = ['113821',
-        #             '523032',
-        #             '130518',
-        #             '151021',
-        #             '130417',
-        #             '130720',
-        #             '202113',
-        #             '188751',
-        #             '118225',
-        #             '284646',
-        #             '120111',
-        #             '123824',
-        #             '268850',
-        #             '123117',
-        #             '368753',
-        #             '161327',
-        #             '176845',
-        #             '159441',
-        #             '559457',
-        #             '115017']
+        self.train_subject_list = ['113821']
+                    # '523032',
+                    # '130518',
+                    # '151021',
+                    # '130417',
+                    # '130720',
+                    # '202113',
+                    # '188751',
+                    # '118225',
+                    # '284646',
+                    # '120111',
+                    # '123824',
+                    # '268850',
+                    # '123117',
+                    # '368753',
+                    # '161327',
+                    # '176845',
+                    # '159441',
+                    # '559457',
+                    # '115017']
         
         #102109 has been removed from the test list due to the fixel directory - can be readded providing the fixles are correct.
         self.val_subject_list = ['104012',  # Subjects to be used for validation
@@ -106,16 +81,12 @@ class NetworkOptions():
         self.dwi_folder_name = 'undersampled_fod'
 
         ### Inference ###
-        # self.test_subject_list = ['145127', # Subjects to be used for testing. **Inference**
-        #     '147737',
-        #     '174437',
-        #     '178849',
-        #     '318637',
-        #     '581450',
-        #     '130821']
-
-        self.test_subject_list = ['145127'] # Subjects to be used for testing. **Inference**
-
-        # self.test_subject_list = ['1', '2', '3']
+        self.test_subject_list = ['145127', # Subjects to be used for testing. **Inference**
+            '147737',
+            '174437',
+            '178849',
+            '318637',
+            '581450',
+            '130821']
 
         print(self.__dict__)

902a1df2f25794aaae28d6368d6e0130c415b5c9
JBartlett6 <j_j_bartlett@hotmail.co.uk>
2024-01-31 01:40:18 +0000

    automatically setting the spatial dimenisons in inference.py accoridng to those in the dataset.
    Updating options.py for Tong inference
    
    Updating the name of NetworkOptions to be consistent with class naming ocnventions.

--- options.py
+++ options.py
@@ -4,13 +4,13 @@ import math
 class NetworkOptions():
     def __init__(self):
         ### General ###
-        self.experiment_name = 'debugging'  # Directory name to be stored in the checkpoint directory. **General**
+        self.experiment_name = 'tong_inference'  # Directory name to be stored in the checkpoint directory. **General**
         self.model_name = 'best_model.pth'  # Working model name - will be stored within the experiment name directory found within the ** General **
                                             # checkpoints directory. 
-        self.continue_training = False      # Whether to continue training using existing model weights
+        self.continue_training = True      # Whether to continue training using existing model weights
 
-        self.inference=False
-        self.perform_inference=False
+        self.inference=True
+        self.perform_inference=True
 
         ### Training Options ###
         self.lr = 1e-4                      # Learning rate - set to this value post learning rate warmup. 
@@ -18,7 +18,7 @@ class NetworkOptions():
         self.warmup_iter = 10000             # Number of iterations after which the network stops warming up.
         self.batch_size = 256               # Training batch_size.
         
-        self.val_freq = 2                 # How often (iterations) to run the validation loop inside the training loop
+        self.val_freq = 500                 # How often (iterations) to run the validation loop inside the training loop
         self.val_iters = 10                 # How many iterations of validation data to loop through when the validation loop is called.
         
         # Stopping Conditions
@@ -46,28 +46,28 @@ class NetworkOptions():
 
         ### Data ###
         # self.data_dir = '/media/duanj/F/joe/hcp_2' # Data directory see github repo for more details.
-        self.data_dir = '/bask/projects/d/duanj-ai-imaging/jxb1336/hcp'
+        self.data_dir = '/bask/projects/d/duanj-ai-imaging/jxb1336/Tong_as_HCP'
 
-        self.train_subject_list = ['113821']
-                    # '523032',
-                    # '130518',
-                    # '151021',
-                    # '130417',
-                    # '130720',
-                    # '202113',
-                    # '188751',
-                    # '118225',
-                    # '284646',
-                    # '120111',
-                    # '123824',
-                    # '268850',
-                    # '123117',
-                    # '368753',
-                    # '161327',
-                    # '176845',
-                    # '159441',
-                    # '559457',
-                    # '115017']
+        self.train_subject_list = ['113821',
+                    '523032',
+                    '130518',
+                    '151021',
+                    '130417',
+                    '130720',
+                    '202113',
+                    '188751',
+                    '118225',
+                    '284646',
+                    '120111',
+                    '123824',
+                    '268850',
+                    '123117',
+                    '368753',
+                    '161327',
+                    '176845',
+                    '159441',
+                    '559457',
+                    '115017']
         
         #102109 has been removed from the test list due to the fixel directory - can be readded providing the fixles are correct.
         self.val_subject_list = ['104012',  # Subjects to be used for validation
@@ -81,12 +81,13 @@ class NetworkOptions():
         self.dwi_folder_name = 'undersampled_fod'
 
         ### Inference ###
-        self.test_subject_list = ['145127', # Subjects to be used for testing. **Inference**
-            '147737',
-            '174437',
-            '178849',
-            '318637',
-            '581450',
-            '130821']
+        # self.test_subject_list = ['145127', # Subjects to be used for testing. **Inference**
+        #     '147737',
+        #     '174437',
+        #     '178849',
+        #     '318637',
+        #     '581450',
+        #     '130821']
+        self.test_subject_list = ['1', '2', '3']
 
         print(self.__dict__)
